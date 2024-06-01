49cb52277c175d653b32249289636e107cad1d58
Dylan <545098+Dylmm@users.noreply.github.com>
2024-05-31 20:29:49 -0700

    Revert "Updated Draw Scripts"
    This reverts commit 3e6210d145a1964c8892d05cc02d54f0b0b346e5.

--- draw-circle.lua
+++ draw-circle.lua
@@ -1,5 +1,5 @@
 -- Aseprite script to draw a circle at a specified (X,Y) with a given radius
--- Written by aquova, 2021. Modified by dylmm, 2024.
+-- Written by aquova, 2021
 -- https://github.com/aquova/aseprite-scripts
 
 -- Open dialog, ask user for paramters
@@ -9,15 +9,15 @@ function userInput()
     dlg:number{ id="x", label="X:", decimals=0 }
     dlg:number{ id="y", label="Y:", decimals=0 }
     dlg:number{ id="radius", label="Radius:", decimals=0 }
-    dlg:check{ id="fill", label="Fill:", selected=true }
     dlg:button{ id="ok", text="OK" }
     dlg:button{ id="cancel", text="Cancel" }
     dlg:show()
+
     return dlg.data
 end
 
 -- Draws the specified circle
-function drawCircle(cx, cy, rad,fill)
+function drawCircle(cx, cy, rad)
     local image = app.activeCel.image
     local copy = image:clone()
     local left = cx - rad
@@ -31,15 +31,9 @@ function drawCircle(cx, cy, rad,fill)
                 dy = dy^2
                 distSquared = dx + dy
                 radSquared = rad^2
-                if fill then   
-                    if distSquared <= radSquared then
-                        copy:drawPixel(x, y, app.fgColor)
-                    end
-                else
-                    if  distSquared + rad >= radSquared and distSquared - rad <= radSquared then
-                        copy:drawPixel(x, y, app.fgColor)
-                    end
-                end 
+                if distSquared <= radSquared then
+                    copy:drawPixel(x, y, app.fgColor)
+                end
             end
         end
     end
@@ -50,6 +44,6 @@ end
 do
     local userCircle = userInput()
     if userCircle.ok then
-        drawCircle(userCircle.x, userCircle.y, userCircle.radius,userCircle.fill)
+        drawCircle(userCircle.x, userCircle.y, userCircle.radius)
     end
 end
